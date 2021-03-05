d729c5d934e71ae20803ab52cb75bfc1c7870601
angelokeirsebilck <angelo.keirsebilck1@telenet.be>
2021-03-05 13:30:50 +0100

    Add server

---
+++ server/app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
+
+const schema = require('./schema/schema');
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_ATLAS_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to database.');
+});
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+    })
+);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Now listening for requests on port ${process.env.PORT}.`);
+});
